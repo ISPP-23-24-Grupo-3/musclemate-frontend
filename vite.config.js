48792b22ee0b9a29da404bff19d6d31152983b5f
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-21 20:52:28 +0100

    feat: add pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
-    }})
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "src/assets/*.png",
+        "src/assets/*.svg",
+        "src/assets/*.ico",
+      ],
+      manifest: {
+        name: "MuscleMate",
+        short_name: "MuscleMate",
+        description: "A new way for gyms and users to connect",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
-})
+});
