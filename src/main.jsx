9ea8624cc9cb906cc073aca53d42aa398d158513
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-22 14:51:03 +0100

    feat: add basic listing for machines

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MachineList from "./MachineList.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
   },
-])
+  {
+    path: "/my-machines",
+    element: <MachineList />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
