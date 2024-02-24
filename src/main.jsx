bdbd7594993df8488bd248b24530094436dcd554
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-24 21:37:09 +0100

    feat: add issue and comment count

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MachineList from "./MachineList.jsx";
+import MachineList from "./views/MachineList.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
