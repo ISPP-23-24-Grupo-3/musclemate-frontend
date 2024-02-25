fa1b175e2d2801203ec002335622d74f72adfd50
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-25 13:45:05 +0100

    fix: removed redundant "Children" parameter

--- src/views/MainLayout/MainLayout.jsx
+++ src/views/MainLayout/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Theme, Container } from "@radix-ui/themes";
 import Footer from "../../components/Footer/Footer";
 import { Outlet } from "react-router";
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
   return (
     <Theme>
       <div className="flex flex-col min-h-screen">
