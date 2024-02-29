355d075dee2d7340fdc9128bb5bd614041b0ac8b
Elena Tomás Vela <eletomvel@alum.us.es>
2024-02-29 21:13:42 +0100

    feat: tweaked routine list aesthetics

--- src/components/UserRoute.jsx
+++ src/components/UserRoute.jsx
@@ -1,17 +1,16 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useContext } from 'react'
-import AuthContext from '../utils/context/AuthContext'
+import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../utils/context/AuthContext";
 
 const UserRoute = () => {
-    let {user} = useContext(AuthContext)
+  let { user } = useContext(AuthContext);
 
-    if (user.rol !== 'user'){
-        return <Navigate to="/login" />
-    }
-    
-    return(
-        <Outlet />
-    )
-}
+  if (user.rol !== "client") {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
 
 export default UserRoute;
+
