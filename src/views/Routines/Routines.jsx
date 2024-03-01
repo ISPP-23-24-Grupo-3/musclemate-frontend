666950d867998f8cf931fd2b233664f1c2f52655
Elena Tomás Vela <eletomvel@alum.us.es>
2024-03-01 22:30:28 +0100

    feat: fetch routines and placeholder adding

--- src/views/Routines/Routines.jsx
+++ src/views/Routines/Routines.jsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
-import { Box, Button, Flex, ScrollArea, Section, Text } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Section,
+  Text,
+  IconButton,
+  Card,
+  TextField,
+} from "@radix-ui/themes";
 import { FaPencil } from "react-icons/fa6";
-import { CgGym } from "react-icons/cg";
+import { CgGym, CgSpinner } from "react-icons/cg";
+import { IoMdAddCircleOutline } from "react-icons/io";
 import { useNavigate } from "react-router";
 import PropTypes from "prop-types";
 import { Error, Info } from "../../components/Callouts/Callouts";
+import { useForm } from "react-hook-form";
 
 export const Routines = () => {
   const [error, setError] = useState("");
@@ -17,37 +29,31 @@ export const Routines = () => {
 
   const fetchRoutines = async () => {
     try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await fetch("/api/routines/");
       if (!response.ok) {
         setError(
-          "There was a problem while searching your routines (Unexpected status code). Please stand by."
+          "There was a problem while searching your routines (Unexpected status code). Please stand by.",
         );
       }
       const routines = await response.json();
       return routines;
     } catch (error) {
       setError(
-        "There was a problem while searching your routines. Please stand by."
+        "There was a problem while searching your routines. Please stand by.",
       );
     }
   };
 
-  const addRoutine = () => navigate("new");
-
   return (
     <Section>
-      <Button
-        type="submit"
-        size="4"
-        variant="solid"
-        m="3"
-        color="green"
-        onClick={addRoutine}
+      <Heading
+        size="8"
+        className="text-radixgreen !mb-3 text-center md:text-left"
       >
-        Add routine
-      </Button>
+        Mis Rutinas
+      </Heading>
+
+      <RoutineForm set_routines={setRoutines} routines={routines} />
 
       {error ? (
         <Error message={error} size="3" />
@@ -61,7 +67,6 @@ export const Routines = () => {
 const ListRoutines = ({ routines }) => {
   const navigate = useNavigate();
   const editRoutine = () => navigate("edit");
-
   const startRoutine = () => navigate("start");
 
   if (routines.length === 0) {
@@ -70,34 +75,68 @@ const ListRoutines = ({ routines }) => {
     );
   }
   return (
-    <ScrollArea scrollbars="vertical" size="2" mx="2" style={{ height: 500 }}>
-      <Flex gap="5" direction="column">
-        {routines.map((routine) => (
-          <Box key={routine.id}>
-            <Flex gap="2">
-              <Box>
-                <Text
-                  style={{ textOverflow: "ellipsis" }}
-                  size="6"
-                  weight="bold"
-                  align="left"
-                >
-                  {routine.title}
-                </Text>
-              </Box>
-              <Box>
-                <Button size="2" mr="2" onClick={editRoutine}>
-                  <FaPencil />
-                </Button>
-                <Button size="2" onClick={startRoutine}>
-                  <CgGym />
-                </Button>
-              </Box>
-            </Flex>
+    <Flex gap="4" direction="column">
+      {routines.map((routine) => (
+        <div
+          key={routine.id}
+          className="bg-radixgreen/10 p-4 flex items-center justify-between rounded-lg"
+        >
+          <Text style={{ textOverflow: "ellipsis" }} size="5" weight="bold">
+            {routine.name}
+          </Text>
+          <Box>
+            <IconButton size="3" mr="2" radius="full" onClick={editRoutine}>
+              <FaPencil />
+            </IconButton>
+            <IconButton size="3" radius="full" onClick={startRoutine}>
+              <CgGym className="size-6 rotate-30" />
+            </IconButton>
           </Box>
-        ))}
-      </Flex>
-    </ScrollArea>
+        </div>
+      ))}
+    </Flex>
+  );
+};
+
+const RoutineForm = ({ set_routines, routines }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [visible_form, set_visible_form] = useState(false);
+  const addRoutine = () => set_visible_form(true);
+
+  const onSubmit = (data) => {
+    set_routines([data, ...routines]);
+    set_visible_form(false);
+    reset();
+  };
+  return (
+    <>
+      <Button
+        className="w-full"
+        type="submit"
+        mb="5"
+        size="3"
+        variant="solid"
+        onClick={addRoutine}
+      >
+        <IoMdAddCircleOutline className="size-6" />
+        Añadir rutina
+      </Button>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`${!visible_form && "hidden"} rounded-lg mb-4 p-5 px-3 flex justify-between border border-radixgreen/30`}
+      >
+        <TextField.Input
+          className="flex-1"
+          {...register("name")}
+        ></TextField.Input>
+        <Button className="">Aceptar</Button>
+      </form>
+    </>
   );
 };
 
@@ -106,6 +145,6 @@ ListRoutines.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
-    })
+    }),
   ).isRequired,
 };
