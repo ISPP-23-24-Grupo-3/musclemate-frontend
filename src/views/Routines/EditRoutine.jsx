990b47ca215041ef74d11592e69bb90083baeeaa
Elena Tomás Vela <eletomvel@alum.us.es>
2024-03-01 14:51:52 +0100

    feat: add form validation

--- src/views/Routines/EditRoutine.jsx
+++ src/views/Routines/EditRoutine.jsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Card,
   Dialog,
@@ -7,7 +6,7 @@ import {
   Heading,
   Text,
   TextField,
-  IconButton,
+  Section,
 } from "@radix-ui/themes";
 import { useState } from "react";
 import { BsQrCodeScan } from "react-icons/bs";
@@ -16,26 +15,26 @@ import { useForm } from "react-hook-form";
 
 import PropTypes from "prop-types";
 
-export const EditRoutine = () => {
-  const workouts = [
-    {
-      id: 1,
-      name: "Press de banca",
-      sets: 4,
-      reps: 3,
-      weight: 12,
-      machine: "Press de banca",
-    },
-    {
-      id: 2,
-      name: "Extensiones de triceps",
-      sets: 4,
-      reps: 3,
-      weight: 24,
-      machine: "Polea",
-    },
-  ];
+const workouts = [
+  {
+    id: 1,
+    name: "Press de banca",
+    sets: 4,
+    reps: 3,
+    weight: 12,
+    machine: "Press de banca",
+  },
+  {
+    id: 2,
+    name: "Extensiones de triceps",
+    sets: 4,
+    reps: 3,
+    weight: 24,
+    machine: "Polea",
+  },
+];
 
+export const EditRoutine = () => {
   const [routine, setRoutine] = useState({
     name: "Routine 1",
     workouts,
@@ -46,33 +45,35 @@ export const EditRoutine = () => {
 
   return (
     <>
-      <div className="flex mt-4 justify-between">
-        <div className="flex items-center gap-3">
-          <Heading>{routine.name}</Heading>
-          <EditDialog updateRoutine={updateRoutine} />
-        </div>
-        <Button size="3">
-          <CgGym className="size-7" />
-          Entrenar
-        </Button>
-      </div>
-      <div className="my-4 bg-radixgreen/40 p-4 rounded-lg">
-        <Heading className="!mb-2">Añadir ejercicio</Heading>
-        <div className="place-content-around gap-20 flex">
-          <Button className="flex-1" variant="surface">
-            Manualmente
-          </Button>
-          <Button className="flex-1" variant="surface">
-            <BsQrCodeScan className="size-5" />
-            Escanea con QR
+      <Section className="!pt-1">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-3">
+            <Heading>{routine.name}</Heading>
+            <EditDialog updateRoutine={updateRoutine} />
+          </div>
+          <Button size="3">
+            <CgGym className="size-7" />
+            Entrenar
           </Button>
         </div>
-      </div>
-      <Heading as="h2">Ejercicios</Heading>
-      <Flex direction="column" gap="3" className="mt-4">
-        <EditableWorkout workout={{}} />
-        <WorkoutList workouts={routine.workouts} />
-      </Flex>
+        <div className="my-4 bg-radixgreen/40 p-4 rounded-lg">
+          <Heading className="!mb-2">Añadir ejercicio</Heading>
+          <div className="place-content-around gap-20 flex">
+            <Button className="flex-1" variant="surface">
+              Manualmente
+            </Button>
+            <Button className="flex-1" variant="surface">
+              <BsQrCodeScan className="size-5" />
+              Escanea con QR
+            </Button>
+          </div>
+        </div>
+        <Heading as="h2">Ejercicios</Heading>
+        <Flex direction="column" gap="3" className="mt-4">
+          <EditableWorkout workout={{}} />
+          <WorkoutList workouts={routine.workouts} />
+        </Flex>
+      </Section>
     </>
   );
 };
@@ -165,17 +166,22 @@ const EditableWorkout = ({ workout }) => {
               <TextField.Input
                 value={workout.name}
                 name="name"
-                {...register("name")}
+                {...register("name", { required: true })}
+                color={errors.name && "red"}
+                className={`${errors.name && "!border-red-500"}`}
               ></TextField.Input>
             </Flex>
             <div className="flex place-content-around w-3/5">
               <Flex direction="column items-center" className="flex-1">
                 <Text weight="bold">Sets</Text>
                 <TextField.Input
-                  className="!w-20"
                   value={workout.sets}
+                  type="number"
+                  min="0"
                   name="sets"
-                  {...register("sets")}
+                  color={errors.sets && "red"}
+                  className={`!w-20 ${errors.sets && "!border-red-500"}`}
+                  {...register("sets", { required: true })}
                 ></TextField.Input>
               </Flex>
               <Flex direction="column items-center" className="flex-1">
@@ -183,7 +189,14 @@ const EditableWorkout = ({ workout }) => {
                 <TextField.Input
                   value={workout.reps}
                   name="reps"
-                  {...register("reps")}
+                  type="number"
+                  min="0"
+                  color={errors.reps && "red"}
+                  className={`!w-20 ${errors.sets && "!border-red-500"}`}
+                  {...register("reps", {
+                    valueAsNumber: "true",
+                    required: true,
+                  })}
                 ></TextField.Input>
               </Flex>
               <Flex direction="column items-center" className="flex-1">
@@ -192,6 +205,10 @@ const EditableWorkout = ({ workout }) => {
                   className="!w-20"
                   value={workout.weight}
                   name="weight"
+                  type="number"
+                  min="0"
+                  color={errors.name && "red"}
+                  className={`${errors.weight && "!border-red-500"}`}
                   {...register("weight")}
                 ></TextField.Input>
               </Flex>
@@ -201,6 +218,8 @@ const EditableWorkout = ({ workout }) => {
               <TextField.Input
                 value={workout.machine}
                 name="machine"
+                color={errors.machine && "red"}
+                className={`${errors.name && "!border-red-500"}`}
                 {...register("machine")}
               ></TextField.Input>
             </Flex>
