b719bedf234dd3298b314bb196231845b2a9cbd2
AMC112002 <alejandromateocapilla@gmail.com>
2024-03-11 20:56:09 +0100

    fix: arreglo ticket listing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cn4lqh2qk7o"
+    "revision": "0.l7iq9in8je"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
