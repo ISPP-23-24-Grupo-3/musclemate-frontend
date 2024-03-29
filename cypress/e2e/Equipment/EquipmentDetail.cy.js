9eb9e2d3ca0abb877525da0b808b51876074c27e
agudevbon <agudevbon@alum.us.es>
2024-03-29 17:59:57 +0100

    test: EquipmentDetail full test

--- cypress/e2e/Equipment/EquipmentDetail.cy.js
+++ cypress/e2e/Equipment/EquipmentDetail.cy.js
@@ -6,17 +6,42 @@ describe('Gym Machine Form', () => {
       cy.get('input[name="password"]').type('musclemate123');
       cy.get('button[type="submit"]').click();
       cy.contains('Mis Máquinas').click();
-      cy.contains('Añadir máquina').click();
+    });
+
+
+    it('debería mostrar los detalles asociados a la maquina', () => {
+      cy.contains('Cinta de correr').click();
+      cy.contains('Cinta de correr').should('be.visible');
+      cy.contains('Cinta de correr plegable con motor de 2.5 HP').should('be.visible');
+      cy.contains('Marca E').should('be.visible');
+      cy.contains('McFIT Sevilla Palacio de Congresos').should('be.visible');
+      cy.contains('Otros').should('be.visible');
+      cy.contains('CDCT005').should('be.visible');
+    });
+
+    it('debería mostrar los tickets asociados a la maquina con tickets', () => {
+      cy.contains('Cinta de correr').click();
+      cy.contains('Ticket 4').should('be.visible');
+      cy.contains('Ticket 1').should('be.visible');
+      cy.contains('No Resuelto').should('be.visible');
+      cy.contains('Resuelto').should('be.visible');
+    });
+
+    it('no debería mostrar los tickets en una maquina sin tickets', () => {
+      cy.contains('Banda de resistencia').click();
+      cy.contains('No hay tickets disponibles.').should('be.visible');
+    });
+
+    it('debería poder editar una maquina', () => {
+      cy.contains('Cinta de correr').click();
+      cy.contains('Editar').click();
+
+      cy.get('input[name="name"]').type('Cinta de correr 2');
+      cy.contains('Guardar').click();
+      cy.contains('Cinta de correr 2').should('be.visible');
     });
   
-    it('debería mostrar un mensaje de error al intentar enviar el formulario con campos vacíos', () => {
-      cy.get('button[type="submit"]').click();
-  
-      // Verificar que se muestren mensajes de error para los campos requeridos
-      cy.contains('Este campo es obligatorio').should('be.visible');
-    });
-  
-    it('debería mostrar un mensaje de error al intentar enviar el formulario con campos inválidos', () => {
+   /* it('debería mostrar un mensaje de error al intentar enviar el formulario con campos inválidos', () => {
       cy.get('input[name="name"]').type('123'); // Nombre corto
       cy.get('button[type="submit"]').click();
       // Verificar que se muestren mensajes de error para campos inválidos
@@ -48,23 +73,7 @@ describe('Gym Machine Form', () => {
   
       // Verificar que se muestren mensajes de error para los campos requeridos
       cy.contains('Este campo es obligatorio').should('be.visible');
-    });
-
-    it('debería crear una nueva máquina de gimnasio correctamente', () => {
-      // Rellenar el formulario con datos válidos
-      cy.get('input[name="name"]').type('Máquina de prueba');
-      cy.get('input[name="brand"]').type('Marca de prueba');
-      cy.get('input[name="serial_number"]').type('123456');
-      cy.get('textarea[name="description"]').type('Descripción de la máquina de prueba');
-      cy.get('select[name="muscular_group"]').select('arms'); // Seleccionar grupo muscular
-      cy.get('select[name="gym"]').select('MasMusculoFit Sevilla MMFit Gym'); // Seleccionar gimnasio existente
-  
-      // Enviar el formulario
-      cy.get('button[type="submit"]').click();
-  
-      // Verificar que se redirige a la página correcta
-      cy.url().should('include', '/owner/equipments');
-    });
+    });*/
   
     // Puedes escribir más pruebas para otros casos según sea necesario
   });
