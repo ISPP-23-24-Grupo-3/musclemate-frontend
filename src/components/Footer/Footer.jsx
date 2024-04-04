60b64a307a38ca4bd07e691e595923ff5a3f3575
Elena Tomás Vela <eletomvel@alum.us.es>
2024-04-04 14:12:37 +0200

    feat: tweaked footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,26 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Link as RLink } from "@radix-ui/themes";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t-2 py-6 px-16 font-[sans-serif]">
+    <footer className="border-t-2 py-6 px-16 font-[sans-serif] bg-slate-200">
       <div className="flex flex-col lg:flex-row items-center justify-between">
         <Flex align="center">
-          <img src="/pwa-64x64.png" alt="Logo" className="mr-4 mb-4 sm:mb-0" />
-          <h1 className="text-xl font-bold">MuscleMate</h1>
+          <img
+            src="/MuscleMateLogo.svg"
+            alt="Logo"
+            className="mr-4 mb-4 sm:mb-0 size-12 brightness-0"
+          />
+          <h1 className="text-xl">MuscleMate</h1>
         </Flex>
         <ul className="flex max-sm:flex-col gap-x-6 gap-y-2">
           <li>
-            <Link to="/terms-conditions"
-              className="hover:text-gray-600 text-black text-base transition-all"
-            >
-              Términos y Condiciones
-            </Link>
+            <RLink>
+              <Link to="/terms-conditions" className="text-base transition-all">
+                Términos y Condiciones
+              </Link>
+            </RLink>
           </li>
           <li>
             <strong>musclemate33@gmail.com</strong>
