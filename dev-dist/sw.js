a47f19b0df12561c0f894b5614b413d8954cbd86
davdelcar <davdelcar@alum.us.es>
2024-04-19 20:23:53 +0200

    fix: Performance improvement

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.btn05255ccg"
+    "revision": "0.87r7jiom458"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
